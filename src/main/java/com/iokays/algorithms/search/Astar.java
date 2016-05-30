ef9e2afc3974af3282cfb427ae85b93ac96eb779
彭元兵 <pengyuanbing@gcall.org>
2016-05-30 14:09:14 +0800

    init

---
+++ src/main/java/com/iokays/algorithms/search/Astar.java
@@ -0,0 +1,13 @@
+package com.iokays.algorithms.search;
+
+/**
+ * A*算法
+ * 
+ * @author pengyuanbing@gmail.com
+ *
+ */
+public final class Astar {
+
+	public static final int[][] move = { { 0, -1 }, { 1, -1 }, { 1, 0 }, { 1, 1 }, { 0, 1 }, { -1, 1 }, { -1, 0 }, { -1, -1 } };
+	
+}
